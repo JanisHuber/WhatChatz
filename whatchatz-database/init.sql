144efc336300ec24774e1d06db798954d40da858
JanisHuber <janis.huber@itme.ch>
2025-07-08 10:16:07 +0200

    project setup

---
+++ whatchatz-database/init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS whatchatz;
+
+USE whatchatz;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    uid  VARCHAR(100) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    info TEXT
+);
+
+CREATE TABLE IF NOT EXISTS messages
+(
+    id          SERIAL PRIMARY KEY,
+    chat_id     VARCHAR(255) NOT NULL,
+    sender_id   VARCHAR(100) NOT NULL,
+    receiver_id VARCHAR(100) NOT NULL,
+    message     TEXT         NOT NULL,
+    timestamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS contacts
+(
+    id                     SERIAL PRIMARY KEY,
+    owner_id               VARCHAR(100) NOT NULL,
+    contact_id             VARCHAR(100) NOT NULL,
+    contact_name           VARCHAR(100) NOT NULL,
+    last_message_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
