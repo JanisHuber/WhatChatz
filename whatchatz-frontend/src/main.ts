eccea6d8251aad733327051be52cfafe3748d43b
JanisHuber <janis.huber@itme.ch>
2025-07-23 12:14:19 +0200

    fixed environment import

--- whatchatz-frontend/src/main.ts
+++ whatchatz-frontend/src/main.ts
@@ -3,10 +3,10 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { firebaseConfig } from './environments/environment';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebaseConfig);
 const auth = getAuth(app);
